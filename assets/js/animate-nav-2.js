4feb38eafdc33ea9b3053a92af48da3c614f43e3
josh-59 <josh.m.timmons@gmail.com>
2021-01-22 15:41:57 -0600

    Brought up-to-date with Learning Linux

--- assets/js/animate-nav-2.js
+++ assets/js/animate-nav-2.js
@@ -1,46 +1,71 @@
-$(document).ready(function () {
-  var screenTop = $(document).scrollTop();
-  var screenBottom = screenTop + $(window).height();
+// To do:
+//      - Preserve aspect ratio of videos across window resizing
+//      - Eliminate nav-bar 'flicker' when navigating across pages in 
+//          narrow-window mode
 
-  $('#nav_bar_2 a').each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
+$(document).ready(highlight_nav);
+$(document).on("scroll", highlight_nav);
 
-      var elemTop = refElement.position().top;
-      var elemBottom = elemTop + refElement.height();
+$(document).ready(check_navbars);
+$(window).resize(check_navbars);
 
-      if ( elemTop >= screenTop && elemTop + 100 < screenBottom ) { // Can see top
-        currLink.addClass("active");
-      } else if ( elemTop < screenTop && elemBottom > screenBottom ) { // Are in the middle
-        currLink.addClass("active");
-      } else if ( elemBottom - 10 > screenTop && elemBottom < screenBottom ) { // Can see bottom
-        currLink.addClass("active");
-      } else {
-          currLink.removeClass("active");
-      }
-  });
-});
+function highlight_nav() {
+    const screenTop = $(document).scrollTop();
+    const screenBottom = screenTop + $(window).height();
 
-$(document).on("scroll", function() {
-    var screenTop = $(document).scrollTop();
-    var screenBottom = screenTop + $(window).height();
+    $('#nav_bar_2 a').each( function () {
+        const currLink = $(this);
+        const refElement = $(currLink.attr("href"));
 
-    $('#nav_bar_2 a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
 
-        var elemTop = refElement.position().top;
-        var elemBottom = elemTop + refElement.height();
-
-        // Note:  .class="content" div's have top padding of 50px.
-        if ( elemTop >= screenTop && elemTop + 100 < screenBottom ) { // Can see top
-          currLink.addClass("active");
-        } else if ( elemTop < screenTop && elemBottom > screenBottom ) { // Are in the middle
-          currLink.addClass("active");
-        } else if ( elemBottom - 10 > screenTop && elemBottom < screenBottom ) { // Can see bottom
-          currLink.addClass("active");
+        if ( refElement.length == 0 ) {
+            // Link exists in nav but corresponding div does not exist
+            ;
         } else {
-            currLink.removeClass("active");
+
+            var elemTop = refElement.position().top;
+            var elemBottom = elemTop + refElement.height();
+
+            if ( elemTop >= screenTop && elemTop + 100 < screenBottom ) {
+                // Can see top
+                currLink.addClass("active");
+            } else if ( elemTop < screenTop && elemBottom > screenBottom ) {
+                // Are in the middle
+                currLink.addClass("active");
+            } else if ( elemBottom - 10 > screenTop && elemBottom < screenBottom ) {
+                // Can see bottom
+                currLink.addClass("active");
+            } else {
+                // Otherwise
+                currLink.removeClass("active");
+            }
         }
     });
-});
+}
+
+// Hides nav_bar_2
+function hide_nav() {
+    $("#nav_bar_2").hide();
+    $(".content").each(function() {
+        $(this).addClass("content-wide");
+    });
+    $("#nav_home").css('width', '120pt');
+}
+
+
+// Shows nav_bar_2
+function show_nav() {
+    $("#nav_bar_2").show();
+    $(".content").each(function() {
+        $(this).removeClass("content-wide");
+    });
+    $("#nav_home").css('width', '240pt');
+}
+
+function check_navbars() {
+    if( window.innerWidth < 1150 ) {
+        hide_nav();
+    } else {
+        show_nav();
+    }
+}
